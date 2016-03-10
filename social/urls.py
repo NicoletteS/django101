d264236ae980e9c3f52ed78780ff5fd46483c223
NicoletteS <nicolettestassen@gmail.com>
2016-03-10 15:47:48 +0100

    New homepage

--- social/urls.py
+++ social/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
 	url(r'sign_up/', views.sign_up, name='sign_up'),
 	url(r'user/add_user', views.add_user, name='add_user'),
 	url(r'bubble/', views.bubble, name="bubble"),
+    url(r'bubble2/', views.bubble2, name="bubble2"),
 ]
